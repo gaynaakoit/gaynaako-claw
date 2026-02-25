33c692bdddad58334ceaec92a805e37fcb16257a
Abdoulaye kane <abdoulaye.kane093@gmail.com>
2026-02-25 03:21:04 +0000

    Add company controller

--- backend/gaynaako-claw-backend/src/modules/company/company.controller.ts
+++ backend/gaynaako-claw-backend/src/modules/company/company.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 
-@Controller('company')
-export class CompanyController {}
+@Controller('companies')
+export class CompanyController {
+  constructor(private readonly service: CompanyService) {}
+
+  @Post()
+  create(@Body() dto: CreateCompanyDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateCompanyDto) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.remove(id);
+  }
+
+  @Post(':id/calculate-scores')
+  async calculateScores(@Param('id') id: string) {
+    const company = await this.service.findOne(id);
+    const scores = this.service['calculateScores'](company); // appelle la fonction privée du service
+    // Optionnel : sauvegarder les scores recalculés
+    Object.assign(company, scores);
+    await this.service.update(id, company);
+    return scores;
+  }
+}
